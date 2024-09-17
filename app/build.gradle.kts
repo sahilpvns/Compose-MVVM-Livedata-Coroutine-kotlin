c13dc596e1dfb08f45d16d6938e595399e0ca817
sahil <sahilpvns@gmail.com>
2024-09-17 20:43:11 +0530

    Compose-MVVM-LiveData-coroutine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,7 +70,7 @@ dependencies {
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.3.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     // Viewmodel
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
