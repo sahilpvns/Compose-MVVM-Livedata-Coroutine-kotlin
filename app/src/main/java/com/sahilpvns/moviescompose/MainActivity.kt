eb2d40f467ff8fd5b2faf74e9c35cbf1a012b157
sahil <sahilpvns@gmail.com>
2024-09-21 22:34:28 +0530

    ui fix

--- app/src/main/java/com/sahilpvns/moviescompose/MainActivity.kt
+++ app/src/main/java/com/sahilpvns/moviescompose/MainActivity.kt
@@ -6,12 +6,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
