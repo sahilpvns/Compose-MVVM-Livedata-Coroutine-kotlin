91f208981dcf75c5ce0663cd269369b607610a6e
sahil <sahilpvns@gmail.com>
2024-09-27 17:24:34 +0530

    remove unused code

--- app/src/main/java/com/sahilpvns/moviescompose/RetrofitClient.kt
+++ app/src/main/java/com/sahilpvns/moviescompose/RetrofitClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
 
-    private const val BASE_URL = "https://www.omdbapi.com/"
+    private const val BASE_URL = "https://www.omdbapi.com"
     val api: Retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
